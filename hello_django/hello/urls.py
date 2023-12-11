4822b27c8800fbd1f57a65604cf18bb0e0c9e386
Karan Gaur <karan.gopal.gaur@gmail.com>
2023-12-10 21:39:42 -0500

    adding-record-log-to-db

--- hello_django/hello/urls.py
+++ hello_django/hello/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("hello/<str:name>/", views.hello_there, name="hello"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("log/", views.log_message, name="log"),
 ]
--- hello_django/hello/views.py
+++ hello_django/hello/views.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+from hello.models import LogMessage
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+
 
 def home(request):
     return render(request, "hello/home.html")
+
 
 def hello_there(request, name):
     print(request.build_absolute_uri())
@@ -12,8 +17,23 @@ def hello_there(request, name):
         'date': datetime.now()
     })
 
+
 def about(request):
     return render(request, "hello/about.html")
 
+
 def contact(request):
     return render(request, "hello/contact.html")
+
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
